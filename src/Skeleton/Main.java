d93f709f8626813759a3f2bb7645b5a68dfb94ba
Bence Orosz <oroszbence0929@gmail.com>
2021-03-15 21:39:18 +0100

    main backbone created

--- src/Skeleton/Main.java
+++ src/Skeleton/Main.java
@@ -1,8 +1,29 @@
 package Skeleton;
 
-public class Main {
-    public static void main(String[] args){
-        System.out.println("hello World!");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) {
+        int choosed = -1;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (choosed != 0) {
+            System.out.println("Kérlek válassz egy opciót!");
+            try {
+                String line = reader.readLine();
+                choosed = Integer.parseInt(String.valueOf(line));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                choosed = 0;
+            }
+            switch (choosed) {
+                case 0:
+                    SkeletonController.Kilepes();
+                    break;
+                case 1:
+                    SkeletonController.TelepesBanyaszikVasat();
+                    break;
+            }
+        }
     }
 }
--- src/Skeleton/SkeletonController.java
+++ src/Skeleton/SkeletonController.java
@@ -2,4 +2,11 @@ package Skeleton;
 
 public class SkeletonController {
 
+    public static void Kilepes(){
+        System.out.println("Bye(t)!");
+    }
+    public static void TelepesBanyaszikVasat(){
+        System.out.println("TelepesBanyaszikVasat");
+
+    }
 }
