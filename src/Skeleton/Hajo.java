d6375fb74d5a70c5b28c9e56b6457fc9b8dea413
AbosaSzakal <abosa99.developer@gmail.com>
2021-03-18 16:13:18 +0100

    Hajo és kiírás keret

--- src/Skeleton/Hajo.java
+++ src/Skeleton/Hajo.java
@@ -1,31 +1,41 @@
 package Skeleton;
 
-public class Hajo {
+abstract class Hajo {
+	
+	protected Aszteroida aszteroida;
+	
     public void Fur(){
-
+		aszteroida.Fur();
     }
-    public void Mozog(Mezo m){
-
+    
+    public void Mozog(Mezo uj_mezo){
+    	SkeletonController.FunctionCall("Hajo", "Mozog()");
+    	uj_mezo.HajoErkezik(this);
+    	
+    	SkeletonController.FunctionReturn();
     }
+    
     public void Napvihar(){
 
     }
-    public void Robbanas(){
-
-    }
-    public void Lepes(){
-
-    }
+    
+    abstract void Robbanas();
+    abstract void Lepes();
     public void Meghal(){
 
     }
-    public void Tetlen(){
-
-    }
+    public void Tetlen(){}
+    
+    //Ezt elbaltáztuk... Aszteroidabeallit kellett volna hogy legyen
     public void MezoBeallit(Mezo m){
-
+    	SkeletonController.FunctionCall("Hajo", "Mezobeallit");
+    			
+    	if(aszteroida != null)
+    		aszteroida.HajoElhagy(this);
+    	aszteroida = (Aszteroida) m;
+    	
+    	SkeletonController.FunctionReturn();
     }
-    public boolean NyerEllenoriz(Epitheto e){
-        return false;
-    }
+    
+    abstract boolean NyerEllenoriz(Epitheto e);
 }
