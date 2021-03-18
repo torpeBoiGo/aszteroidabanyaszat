56cbebf562ee82d685cda899d15a9d2c8d230888
maya2d26 <78872766+maya2d26@users.noreply.github.com>
2021-03-18 16:37:32 +0100

    nyersanyag

--- src/Skeleton/Nyersanyag.java
+++ src/Skeleton/Nyersanyag.java
@@ -1,4 +1,15 @@
 package Skeleton;
 
 public class Nyersanyag {
+  public void Megsemmisul() {
+		
+	} 
+	
+	public void Megfurva(Aszteroida a) {
+		
+	}
+	
+	public boolean KompatibilisE(Nyersanyag ny) {
+		return (equals(ny));
+	}
 }
