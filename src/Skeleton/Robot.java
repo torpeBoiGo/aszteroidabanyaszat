4638bdf3e9459abb5993c9f36f7730f3b0a77f4f
trombitasp <trombitasp@gmail.com>
2021-03-19 13:34:17 +0100

    robot és telepes függvények létrehozva

--- src/Skeleton/Robot.java
+++ src/Skeleton/Robot.java
@@ -1,4 +1,23 @@
 package Skeleton;
 
-public class Robot {
+public class Robot extends Hajo {
+
+	@Override
+	void Robbanas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void Lepes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	boolean NyerEllenoriz(Epitheto e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
