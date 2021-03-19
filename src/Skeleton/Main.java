d3f413b18d072a8d351ed37582d830365b84120c
AbosaSzakal <abosa99.developer@gmail.com>
2021-03-19 17:33:01 +0100

    Jobb menü, keretrendszer javítások

--- src/Skeleton/Main.java
+++ src/Skeleton/Main.java
@@ -3,15 +3,24 @@ package Skeleton;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
         int chosen = -1;
+        ArrayList<MenuItem> menuItems= new ArrayList<MenuItem>();
+        menuItems.add(new MenuItem("Kilepes", SkeletonController::Kilepes));
+        menuItems.add(new MenuItem("Telepes mozog szomszedos aszteroidara", SkeletonController::TelepesMozogSzomszedosAszteroidara));
+        
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (chosen != 0) {
             System.out.println("Kerlek valassz egy opciot!\n");
-            System.out.println("0. Kilepes");
-            System.out.println("1. Telepes mozog szomszedos aszteroidara");
+            
+            for (int i = 0; i< menuItems.size();i++) 
+            	 System.out.println(i + ". " + menuItems.get(i).name);
+			
+           
             System.out.print("Valasztas: ");
             try {
                 String line = reader.readLine();
@@ -22,17 +31,12 @@ public class Main {
             }
             
             try {
-            	switch (chosen) {
-                case 0:
-                    SkeletonController.Kilepes();
-                    break;
-                case 1:
-                    SkeletonController.TelepesMozog();
-                    break;
-            	}
+            	menuItems.get(chosen).toCall.run();
 			} catch (Exception e) {
 				
 			}
+            
+            SkeletonController.NamesMap.clear();
             
         }
     }
--- src/Skeleton/Robot.java
+++ src/Skeleton/Robot.java
@@ -1,4 +1,26 @@
 package Skeleton;
 
-public class Robot {
+public class Robot extends Hajo{
+	public Robot(Aszteroida a) {
+        SkeletonController.ObjectCreated(this);
+        a.HajoErkezik(this);
+        aszteroida = a;
+    }
+
+	@Override
+	void Robbanas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void Lepes() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	boolean NyerEllenoriz(Epitheto e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
