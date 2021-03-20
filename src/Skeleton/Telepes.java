32a69bbea368856c111b34ab3133654ed8f6aa59
trombitasp <78873457+trombitasp@users.noreply.github.com>
2021-03-20 21:55:06 +0100

    Update Telepes.java

--- src/Skeleton/Telepes.java
+++ src/Skeleton/Telepes.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-import java.util.ArrayList;
-
 public class Telepes extends Hajo{
 
-	ArrayList<Nyersanyag> Rakter = new ArrayList<Nyersanyag>();
 
 
     List<Szallithato> nyersanyagRakter = new ArrayList<Szallithato>();
